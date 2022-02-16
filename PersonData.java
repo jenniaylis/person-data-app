cbf5a4cdfc6bdb3a8f3d66d9b32467ee57831f3e
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-02-16 14:41:26 +0200

    Added graphic UI with swing

--- PersonData.java
+++ PersonData.java
@@ -1,63 +1,143 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class PersonData {
-    public static void main (String args[]) {
-        ArrayList<Person> persones = new ArrayList<Person>();
 
-        while (true) {
-            Scanner myScanner = new Scanner(System.in);
+    JPanel textPanel, panelForTextFields;
+    JLabel titleLabel, nameLabel, ageLabel, streetLabel, postcodeLabel, cityLabel;
+    JTextField nameField, ageField, streetField, postcodeField, cityField;
+    JButton submitButton;
+    static ArrayList<Person> persones = new ArrayList<Person>();
 
-            System.out.println("Choose the function by inputting number: 1 add new person, 2 delete person, 3 print all persons, 4 exit");
-            String input = myScanner.nextLine();
+    public JPanel createContentPane (){
 
-            if (input.equals("1")) { // add new person
-                System.out.print("Name: ");
-                String name = myScanner.nextLine();
+        // We create a bottom JPanel to place everything on.
+        JPanel totalGUI = new JPanel();
+        totalGUI.setLayout(null);
+        totalGUI.setBackground(new Color(175,238,238));
 
-                System.out.print("Age: ");
-                String ageInput = myScanner.nextLine();
+        titleLabel = new JLabel("Add new person");
+        titleLabel.setLocation(0,0);
+        titleLabel.setSize(290, 30);
+        titleLabel.setHorizontalAlignment(0);
+        totalGUI.add(titleLabel);
 
-                System.out.print("Street: ");
-                String street = myScanner.nextLine();
+        // Label Panel Container
+        textPanel = new JPanel();
+        textPanel.setLayout(null);
+        textPanel.setLocation(10, 35);
+        textPanel.setSize(70, 200);
+        textPanel.setBackground(new Color(175,238,238));
+        totalGUI.add(textPanel);
 
-                System.out.print("Postcode: ");
-                String postcodeInput = myScanner.nextLine();
+        // Name Label
+        nameLabel = new JLabel("Name");
+        nameLabel.setLocation(0, 0);
+        nameLabel.setSize(70, 40);
+        nameLabel.setHorizontalAlignment(4);
+        textPanel.add(nameLabel);
 
-                System.out.print("City: ");
-                String city = myScanner.nextLine();
+        // Age Label
+        ageLabel = new JLabel("Age");
+        ageLabel.setLocation(0, 40);
+        ageLabel.setSize(70, 40);
+        ageLabel.setHorizontalAlignment(4);
+        textPanel.add(ageLabel);
 
-                int age = Integer.valueOf(ageInput);
-                int postcode = Integer.valueOf(postcodeInput);
-                Address newAddress = new Address(street, postcode, city);
+        // Street Label
+        streetLabel = new JLabel("Street");
+        streetLabel.setLocation(0, 80);
+        streetLabel.setSize(70, 40);
+        streetLabel.setHorizontalAlignment(4);
+        textPanel.add(streetLabel);
 
-                System.out.println();
+        // Postcode Label
+        postcodeLabel = new JLabel("Postcode");
+        postcodeLabel.setLocation(0, 120);
+        postcodeLabel.setSize(70, 40);
+        postcodeLabel.setHorizontalAlignment(4);
+        textPanel.add(postcodeLabel);
 
-                persones.add(new Person(name, age, newAddress));
+        // City Label
+        cityLabel = new JLabel("City");
+        cityLabel.setLocation(0, 160);
+        cityLabel.setSize(70, 40);
+        cityLabel.setHorizontalAlignment(4);
+        textPanel.add(cityLabel);
+
+        // TextFields Panel Container
+        panelForTextFields = new JPanel();
+        panelForTextFields.setLayout(null);
+        panelForTextFields.setLocation(110, 40);
+        panelForTextFields.setSize(100, 200);
+        panelForTextFields.setBackground(new Color(175,238,238));
+        totalGUI.add(panelForTextFields);
+
+        // Name Textfield
+        nameField = new JTextField(8);
+        nameField.setLocation(0, 0);
+        nameField.setSize(100, 30);
+        panelForTextFields.add(nameField);
+
+        // Age Textfield
+        ageField = new JTextField(8);
+        ageField.setLocation(0, 40);
+        ageField.setSize(100, 30);
+        panelForTextFields.add(ageField);
+
+        // Street Textfield
+        streetField = new JTextField(8);
+        streetField.setLocation(0, 80);
+        streetField.setSize(100, 30);
+        panelForTextFields.add(streetField);
+
+        // Postcode Textfield
+        postcodeField = new JTextField(8);
+        postcodeField.setLocation(0, 120);
+        postcodeField.setSize(100, 30);
+        panelForTextFields.add(postcodeField);
+
+        // City Textfield
+        cityField = new JTextField(8);
+        cityField.setLocation(0, 160);
+        cityField.setSize(100, 30);
+        panelForTextFields.add(cityField);
+
+        // Button for Submitting the information given
+        submitButton = new JButton("Submit");
+        submitButton.setLocation(130, 240);
+        submitButton.setSize(80, 30);
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int postcode, age;
+                age=20;
+                postcode = 89; // (dont know, how to turn this textfield into integer value...)
+                Address newAddress = new Address(streetField.toString(), postcode, cityField.toString());
+                persones.add(new Person(nameField.toString(), age, newAddress));
             }
-            else if (input.equals("2")) { // remove person
+        });
+        totalGUI.add(submitButton);
 
-                System.out.println("Who do you want to delete?");
-                String name = myScanner.nextLine();
+        totalGUI.setOpaque(true);    
+        return totalGUI;
+    }
+    private static void createAndShowGUI() {
 
-                for (int i = 0; i < persones.size(); i++) {
-                    if (name == persones.get(i).getName()) {
-                        persones.remove(i);
-                    }		
-                }
-                System.out.println("");
-            }
-            else if (input.equals("3")) { // print all persons
-                for (Person p : persones) {
-	                p.printPerson();
-                    System.out.println("");	
-	            }   
-            }
-            else if (input.equals("4")) { // exit
-                break;
-            } else {
-                System.out.println("Give number between 1 and 4");
-            }
-        }
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("Person Form App");
+
+        PersonData demo = new PersonData();
+        frame.setContentPane(demo.createContentPane());
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 400);
+        frame.setVisible(true);
+    }
+    public static void main( String args[] ){
+        createAndShowGUI();
     }
 }
